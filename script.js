61f21f1a948277ecee3d3260b4316bffbbe5fcb5
Saeed <70399430+Elhammsi@users.noreply.github.com>
2023-06-17 20:38:18 +0200

    Reformat the js script

--- script.js
+++ script.js
@@ -1,113 +1,106 @@
-var form = document.querySelector('.form');
-var move = document.querySelector('.move');
-const inputbook = document.querySelector('.inputbook');
-const inputaddress = document.querySelector('.inputaddress');
-const inputselect = document.querySelector('.inputselect');
+var form = document.querySelector(".form");
+var move = document.querySelector(".move");
+const inputbook = document.querySelector(".inputbook");
+const inputaddress = document.querySelector(".inputaddress");
+const inputselect = document.querySelector(".inputselect");
 const nbook = +inputbook.value;
 const address = inputaddress.value.trim();
 const select = inputselect.value;
 
 class Bookcase {
-
-    constructor(coords, address, nbook) {
-        this.coords = coords;
-        this.address = address;
-        this.nbook = nbook;
-    }
-
+  constructor(coords, address, nbook) {
+    this.coords = coords;
+    this.address = address;
+    this.nbook = nbook;
+  }
 }
+
 class App {
-    #map;
-    #mapEvent;
-    #bookcase = [];
-    constructor() {
-        this._getPosition();
-        form.addEventListener('submit', this._newBookcase.bind(this));
-        move.addEventListener('click', this._move);
+  #map;
+  #mapEvent;
+  #bookcase = [];
+  constructor() {
+    this._getPosition();
+    form.addEventListener("submit", this._newBookcase.bind(this));
+    move.addEventListener("click", this._move);
+  }
 
-    };
-
-    _getPosition() {
-        if (navigator.geolocation)
-            navigator.geolocation.getCurrentPosition(
-                this._loadMap.bind(this),
-                function () {
-                    alert('Could not get your position');
-                }
-            );
-    }
-    _loadMap(position) {
-
-        const { latitude } = position.coords;
-        const { longitude } = position.coords;
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-        const coords = [latitude, longitude];
-        console.log(this);
-        this.#map = L.map('map').setView(coords, 12);
-
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(this.#map);
-
-        this.#map.on('click', this._showForm.bind(this));
-
-    }
-    _showForm(mapE) {
-        this.#mapEvent = mapE
-        form.classList.remove('hide');
-
-    }
-
-
-    _newBookcase(e) {
-        e.preventDefault(); // Prevent form submission and page reload
-
-        const nbook = +inputbook.value;
-        const address = inputaddress.value.trim();
-        const select = inputselect.value;
-        // Create a new instance of the Bookcase class with the provided values
-
-
-        // Check if number of books is positive
-        if (!Number.isFinite(nbook) || nbook <= 0) {
-            alert('Number of books should be a positive number!');
-            return;
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("Could not get your position");
         }
+      );
+  }
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    const coords = [latitude, longitude];
+    console.log(this);
+    this.#map = L.map("map").setView(coords, 12);
 
-        // Check if address is filled
-        if (address === '') {
-            alert('Address field should be filled!');
-            return;
-        }
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
 
-        const { lat, lng } = this.#mapEvent.latlng;
-        const bookcase = new Bookcase([lat, lng], address, nbook);
-        bookcase.select = select;
-        L.marker([lat, lng])
-            .addTo(this.#map)
-            .bindPopup(
-                L.popup({
-                    maxWidth: 250,
-                    minWidth: 100,
-                    autoClose: false,
-                    closeOnClick: false,
-                })
-            )
-            .setPopupContent(`${bookcase.select}`)
-            .openPopup();
+    this.#map.on("click", this._showForm.bind(this));
+  }
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove("hide");
+  }
 
-        form.classList.add('hide');
+  _newBookcase(e) {
+    e.preventDefault(); // Prevent form submission and page reload
 
+    const nbook = +inputbook.value;
+    const address = inputaddress.value.trim();
+    const select = inputselect.value;
+    // Create a new instance of the Bookcase class with the provided values
 
-        this.#bookcase.push(bookcase);
-        this._renderbookcase(bookcase);
-        // Clear input values
-        inputbook.value = '';
-        inputaddress.value = '';
-        inputselect.value = '';
+    // Check if number of books is positive
+    if (!Number.isFinite(nbook) || nbook <= 0) {
+      alert("Number of books should be a positive number!");
+      return;
     }
-    _renderbookcase(bookcase) {
-        let html = `
+
+    // Check if address is filled
+    if (address === "") {
+      alert("Address field should be filled!");
+      return;
+    }
+
+    const { lat, lng } = this.#mapEvent.latlng;
+    const bookcase = new Bookcase([lat, lng], address, nbook);
+    bookcase.select = select;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+        })
+      )
+      .setPopupContent(`${bookcase.select}`)
+      .openPopup();
+
+    form.classList.add("hide");
+
+    this.#bookcase.push(bookcase);
+    this._renderbookcase(bookcase);
+    // Clear input values
+    inputbook.value = "";
+    inputaddress.value = "";
+    inputselect.value = "";
+  }
+  _renderbookcase(bookcase) {
+    let html = `
         
          <div class="card" >
             <div class="card-header">
@@ -122,14 +115,11 @@ class App {
         
        
         `;
-        form.insertAdjacentHTML('afterend', html);
-
-    }
-    _move(e) {
-        const moveEl = e.target.closest('.card');
-        if (!moveEl) return;
-
-    }
+    form.insertAdjacentHTML("afterend", html);
+  }
+  _move(e) {
+    const moveEl = e.target.closest(".card");
+    if (!moveEl) return;
+  }
 }
 const app = new App();
-
